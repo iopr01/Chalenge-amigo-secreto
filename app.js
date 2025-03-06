65433784d21d300b00b726352dbfe665964e8d36
iopr01 <99674637+iopr01@users.noreply.github.com>
2025-03-06 14:06:39 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+        
+        function agregarAmigo() {
+            let input = document.getElementById("amigo");
+            let amigo = input.value.trim();
+            
+            if (amigo === "") {
+                alert("Debe introducir un amigo");
+                return;
+            } else {
+
+                nombres.push(amigo);
+                let lista = document.getElementById("listaAmigos");
+                let li = document.createElement("li");
+                li.textContent = amigo;
+                lista.appendChild(li);
+                input.value = "";
+                mostrarListaAmigo();
+
+            }
+            
+            
+            
+        }
+
+    
+        
+        function sortearAmigo() {
+            if (nombres.length < 2) {
+                alert("Debe haber al menos dos participantes para sortear");
+                return;
+            }else {
+
+                let indice = Math.floor(Math.random() * nombres.length);
+            document.getElementById("resultado").textContent = "El amigo secreto es: " + nombres[indice];
+
+            }
+            
+        }
